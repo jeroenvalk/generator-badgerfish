cf4b8a6463757afaed83d3a09ff373b514197be2
Jeroen Valk <jeroen@agentsatwork.nl>
2015-10-21 17:25:34 +0200

    added Gruntfile template

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,13 +2,13 @@ var generators = require('yeoman-generator');
 module.exports = generators.Base.extend({
 	writing : function() {
 		var self = this;
-		['package.json', 'pom.xml'].forEach(function(fname) {
+		[ 'package.json', 'pom.xml', 'Gruntfile.js', 'Gruntfile.json' ].forEach(function(fname) {
 			self.fs.copyTpl(self.templatePath(fname), self.destinationPath(fname), {
 				artifactId : '',
-				version: '0.1.0-SNAPSHOT',
-				repo: '',
-				author: ''
-			});			
+				version : '0.1.0-SNAPSHOT',
+				repo : '',
+				author : ''
+			});
 		});
 	}
 });
